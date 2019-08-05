f95dcfbd9ce76babc7effd1667ddb277d7090823
Clarisse U <clarisseum@gmail.com>
2019-08-05 17:10:58 +0200

    display the summary

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
     $("#btnSubmit").click(function(event) {
         event.preventDefault();
         var sizeOfPizza = $('#size').val();
+        console.log(sizeOfPizza);
+        // document.getElementById('sizeP').innerHTML = sizeOfPizza;
         var pizzaCrust = $('#crust').val();
+        console.log(pizzaCrust);
+        // document.getElementById('crustP').innerHTML = pizzaCrust;
         var toppings = [];
         $("input[type = checkbox]:checked").each(function() {
             toppings.push($(this).val())
@@ -12,8 +16,14 @@ $(document).ready(function() {
             top += parseInt(toppings[i]);
         }
         var total = parseInt(sizeOfPizza) + parseInt(pizzaCrust) + parseInt(top);
-        alert("the total is:" + total);
-
-
+        alert("the total is: " + total);
+        $('#size').text(total);
+        $('#sizeP').append("<td>" + sizeOfPizza + "</td>");
+        $('#crustP').append("<td>" + pizzaCrust + "</td>");
+        $('#toppz').append("<td>" + top + "</td>")
+    });
+    $("#btnAddMore").click(function(event) {
+        event.preventDefault();
+        var c = location.reload(forceGet);
     });
 });
